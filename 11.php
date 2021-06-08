739f5ebf020cbbad188da06dc2f24d33e81d13ec
EvelinaSha <85555062+EvelinaSha@users.noreply.github.com>
2021-06-08 13:52:08 +0300

    Add files via upload

---
+++ 11.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div id="wrapper">
+    <div id="header">
+      <h1>Грузоперевозки</h1>
+        <p class="nomer">234-49-50 <br> +7 900 650 33 45</p>
+      <img src="http://trueimages.ru/img/cf/26/9116df15.png">
+    </div>
+</body>
+</html>
